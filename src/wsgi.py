3874b12556116fc57d3b2cf06b5c23fcd285fe2d
bim <timotheuzi@yahoo.com>
2026-03-31 00:56:14 -0500

    updates to homepage

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,25 @@
 """WSGI entry point for PythonAnywhere deployment."""
 
-from app import app as application
+import sys
+import os
 
-if __name__ == "__main__":
-    application.run()
+# Try multiple possible project locations
+possible_paths = [
+    '/home/maggotcorp/maggotcorp-homepage/src',
+    '/home/maggotcorp/python_homepage/src',
+    '/home/maggotcorp/homepage/src',
+    '/home/maggotcorp/src',
+]
+
+for path in possible_paths:
+    if os.path.exists(path) and path not in sys.path:
+        sys.path.insert(0, path)
+        break
+
+# Also try to find the src directory relative to the WSGI file
+wsgi_dir = os.path.dirname(os.path.abspath(__file__))
+src_dir = os.path.join(os.path.dirname(wsgi_dir), 'src')
+if os.path.exists(src_dir) and src_dir not in sys.path:
+    sys.path.insert(0, src_dir)
+
+from app import app as application
